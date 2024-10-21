85dff463bdbb79c0efb6f064cdcb2743dc72faa5
Andrew Pan <andrewcypan@gmail.com>
2024-10-21 09:50:27 -0400

    astroworld

---
+++ myproject/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def register(request):
+    context = {
+        "active_link": "register"
+    }
+    
+    return render(request, 'users/register.html', context)
+# Create your views here.
